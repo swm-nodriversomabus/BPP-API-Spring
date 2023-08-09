62f06faa9e82f4823d6fd5100ce206995e1b3aa8
namhyo01 <namhyo01@naver.com>
2023-08-09 12:53:47 +0900

    feat(BE): swagger추가 BUS-88-ChatRoomList #13

--- src/main/java/com/example/api/chatroom/repository/ChatRoomRepository.java
+++ src/main/java/com/example/api/chatroom/repository/ChatRoomRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoomEntity, UUID> {
-    @Query(value = "select DISTINCT c from ChatRoomEntity c join fetch c.members e where e.userId = :userid")
+    @Query(value = "select DISTINCT c from ChatRoomEntity c join fetch c.members e where e.userId = :userid") // n+1 문제 떄문에 fetch join 사용
     List<ChatRoomEntity> findAllByUserId(@Param("userid") Long userid);
 }
