36f7f30d897bc8172ac7c49bf913e51e89116edc
Lemonade255 <tlsdydaud1@naver.com>
2023-08-12 16:48:09 +0900

    feat(BE): 매칭 CRUD 수정 BUS-120-Matching-service #11

--- src/main/java/com/example/api/chatroom/application/port/in/CreateChatRoomUsecase.java
+++
@@ -1,7 +0,0 @@
-package com.example.api.chatroom.application.port.in;
-
-import com.example.api.chatroom.dto.CreateChatRoomDto;
-
-public interface CreateChatRoomUsecase {
-    void createRoom(CreateChatRoomDto createChatRoomDto);
-}
