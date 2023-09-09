3dba7a480618ba50350407f2553a3bf9775aa7ba
namhyo01 <namhyo01@naver.com>
2023-09-10 03:11:27 +0900

    feat(BE): service port 연동  #33 BUS-91-Oauth-Google

--- src/main/java/com/example/api/auth/service/RefreshTokenService.java
+++ src/main/java/com/example/api/auth/service/RefreshTokenService.java
@@ -1,6 +1,7 @@
 package com.example.api.auth.service;
 
 
+import com.example.api.auth.application.port.in.FindRefreshUsecase;
 import com.example.api.auth.application.port.in.LogoutUsecase;
 import com.example.api.auth.repository.RefreshTokenRepository;
 import com.example.api.auth.type.RefreshToken;
@@ -8,18 +9,39 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class RefreshTokenService implements LogoutUsecase {
+public class RefreshTokenService implements LogoutUsecase, FindRefreshUsecase {
     private final RefreshTokenRepository repository;
 
+    /**
+     * 토큰 저장
+     * @param id
+     * @param accessToken
+     * @param refreshToken
+     */
     @Transactional
     public void saveToken(String id, String accessToken, String refreshToken) {
         repository.save(new RefreshToken(id, accessToken, refreshToken));
     }
 
+    /**
+     * 토큰 저장 2 - 위랑 다른점은 refresh token차이점
+     * @param token
+     */
+    @Override
+    @Transactional
+    public void save(RefreshToken token) {
+        repository.save(token);
+    }
 
+    @Override
+    public Optional<RefreshToken> findByAccessToken(String accessToken) {
+        return repository.findByAccessToken(accessToken);
+    }
 
     @Override
     @Transactional
