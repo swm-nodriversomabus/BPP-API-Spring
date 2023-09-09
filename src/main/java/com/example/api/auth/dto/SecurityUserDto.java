15cc7ebbdf7998c4ff7f79d6af98602c4a4375bc
namhyo01 <namhyo01@naver.com>
2023-09-10 03:12:13 +0900

    feat(BE): security user dto 추가 #33 BUS-91-Oauth-Google

---
+++ src/main/java/com/example/api/auth/dto/SecurityUserDto.java
@@ -0,0 +1,29 @@
+package com.example.api.auth.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class SecurityUserDto {
+    @NotNull
+    private Long userId;
+
+    private String naverId;
+
+    private String kakaoId;
+
+    private String googleId;
+
+    private String instaId;
+
+    private String appleId;
+
+    @NotNull
+    private String role;
+
+    @NotNull
+    private String nickname;
+
+}
