0071c65a98cc145ad4ee8c0f4694fc21e345c3d7
namhyo01 <namhyo01@naver.com>
2023-09-27 20:53:39 +0900

    feat(BE): Session Redis 추가 #87 #FIX-87-authroize-errpr

--- src/main/java/com/example/api/ApiApplication.java
+++ src/main/java/com/example/api/ApiApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableRedisHttpSession
 @EnableJpaAuditing
 public class ApiApplication {
 	public static void main(String[] args) {
