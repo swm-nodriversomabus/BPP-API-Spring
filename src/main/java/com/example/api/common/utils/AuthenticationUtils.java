1f419019ed8901c10d8c6b6b7dd100ccfee0888a
Lemonade255 <tlsdydaud1@naver.com>
2023-10-19 14:09:40 +0900

    feat(BE): 로그인한 사용자 자동 인식 #115

--- src/main/java/com/example/api/common/utils/AuthenticationUtils.java
+++ src/main/java/com/example/api/common/utils/AuthenticationUtils.java
@@ -1,18 +1,16 @@
 package com.example.api.common.utils;
 
 import com.example.api.auth.domain.SecurityUser;
-import com.example.api.user.domain.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-
 
 /**
  * 이걸로 유저 정보를 가져오자
  */
 public class AuthenticationUtils {
-    public static SecurityUser getCurrentUserAuthentication(){
+    public static SecurityUser getCurrentUserAuthentication() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof SecurityUser){
+        if (authentication != null && authentication.getPrincipal() instanceof SecurityUser) {
             return (SecurityUser) authentication.getPrincipal();
         }
         return null;
