0981d131724b40be9fb006654df04346a1279a35
Lemonade255 <tlsdydaud1@naver.com>
2023-11-13 13:11:49 +0900

    fix(BE): 사용자 추가 접근 제한 해제 BUS-203-Authority-check

--- src/main/java/com/example/api/user/adapter/in/rest/UserController.java
+++ src/main/java/com/example/api/user/adapter/in/rest/UserController.java
@@ -64,11 +64,6 @@ public class UserController {
     @Operation(summary = "Create user", description = "새로운 사용자를 추가한다.")
     @PostMapping("/user")
     public void createUser(@Valid @RequestBody CreateUserDto userDto, BindingResult bindingResult) {
-        SecurityUser securityUser = AuthenticationUtils.getCurrentUserAuthentication();
-        if (securityUser == null) {
-            log.error("UserController::createUser: Login is needed");
-            throw new CustomException(ErrorCodeEnum.LOGIN_IS_NOT_DONE);
-        }
         UUID userId = saveUserUsecase.createUser(userDto);
         profileImageUsecase.initializeProfileImage(userId);
     }
