c40a881b1aa2453689cce518333f487bc5e8aa67
namhyo01 <namhyo01@naver.com>
2023-11-22 20:20:10 +0900

    hotfix(BE): 친구추가 버그 수정

--- src/main/java/com/example/api/friend/adapter/out/persistence/FriendPK.java
+++ src/main/java/com/example/api/friend/adapter/out/persistence/FriendPK.java
@@ -2,12 +2,14 @@ package com.example.api.friend.adapter.out.persistence;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class FriendPK implements Serializable {
     private UUID userId;
     private UUID friendId;
